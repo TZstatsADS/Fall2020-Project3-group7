965da2431d1a262e5d976aa38f679ea67e0c6d85
cu20-dk3112 <70994813+cu20-dk3112@users.noreply.github.com>
2020-10-31 23:23:05 -0400

    improved RandomForest

--- lib/rand_f.R
+++ lib/rand_f.R
@@ -2,30 +2,37 @@
 library(randomForest)
 
 rf_tune <- function(df){
-  tunex = df[, -6007]
+  set.seed(0)
+  tunex = as.matrix(df[, -6007])
   tuney = factor(df[, 6007])
   return(
     tuneRF(x = tunex,
-           y = tuney)
+           y = tuney,
+           ntreeTry = 750,
+           improve = 0.1,
+           stepFactor = 1)
   )
 }
 
 
 rf_train <- function(df, mtry){
-  trainx = df[, -6007]
+  trainx = as.matrix(df[, -6007])
   trainy = factor(df[, 6007])
-  set.seed(2020)
+  set.seed(0)
   return(
     randomForest(x = trainx,
                  y = trainy,
                  mtry = mtry,
+                 ntree = 1000,
+                 sampsize = nrow(trainx),
+                 nodesize = 25
     )
   )
 }
 
 rf_test <- function(model, testdf){
-  test.x = testdf[ , -6007]
-  set.seed(2020)
+  test.x = as.matrix(testdf[ , -6007])
+  set.seed(0)
   return(
     predict(model, test.x)
   )
