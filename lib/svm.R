ca64815f3fb372b9286041633a0ba72779844ff6
cu20-dk3112 <70994813+cu20-dk3112@users.noreply.github.com>
2020-11-01 00:00:09 -0400

    improve performance

--- lib/svm.R
+++ lib/svm.R
@@ -4,32 +4,37 @@
 
 library(e1071)
 
-svm_tune <- function(df){
-  tunex = df[, -6007]
-  tuney = factor(df[, 6007])
+svm_tune <- function(train.df, test.df){
+  tunex = NormalizeObject(train.df[, -6007])
+  tuney = factor(train.df[, 6007])
+  valix = NormalizeObject(test.df[, -6007])
+  valiy = factor(test.df[, 6007])
   set.seed(2020)
   return(
-    tune(svm, x = tunex,
-         y = tuney,
+    tune(svm, train.x = tunex,
+         train.y = tuney,
+         validation.x = valix,
+         validation.y = valiy,
          ranges = list(epsilon = seq(0,1,0.1),
                        cost = 2^(2:9)))
   )
 }
 
-svm_train <- function(df, mtry){
-  trainx = df[, -6007]
+svm_train <- function(df){
+  trainx = NormalizeObject(df[, -6007])
   trainy = factor(df[, 6007])
   set.seed(2020)
   return(
-    randomForest(x = trainx,
-                 y = trainy,
-                 mtry = mtry,
+    svm (x = trainx,
+         y = trainy,
+         kernel = "sigmoid",
+         cross = 50
     )
   )
 }
 
 svm_test <- function(model, testdf){
-  test.x = testdf[ , -6007]
+  test.x = NormalizeObject(testdf[ , -6007])
   set.seed(2020)
   return(
     predict(model, test.x)
