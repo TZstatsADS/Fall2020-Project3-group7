8701dcd67accc52b3b76337d3fa98206e52a0484
cu20-dk3112 <70994813+cu20-dk3112@users.noreply.github.com>
2020-11-01 21:56:55 -0500

    SVM bug fix (patience needed)

--- lib/svm.R
+++ lib/svm.R
@@ -4,38 +4,36 @@
 
 library(e1071)
 
-svm_tune <- function(train.df, test.df){
-  tunex = NormalizeObject(train.df[, -6007])
-  tuney = factor(train.df[, 6007])
-  valix = NormalizeObject(test.df[, -6007])
-  valiy = factor(test.df[, 6007])
-  set.seed(2020)
+svm_tune <- function(df){
+  set.seed(0)
   return(
-    tune(svm, train.x = tunex,
-         train.y = tuney,
-         validation.x = valix,
-         validation.y = valiy,
-         ranges = list(epsilon = seq(0,1,0.1),
-                       cost = 2^(2:9)))
+    tune(method = svm,
+         train.x = as.matrix(df[,-6007]),
+         train.y = factor(df$label),
+         kernel = "linear",
+         ranges = list(
+           gamma = seq(0.04, 0.12, 0.02),
+           cost = seq(0.02, 0.1, 0.02)
+         ))
   )
 }
 
-svm_train <- function(df){
-  trainx = NormalizeObject(df[, -6007])
-  trainy = factor(df[, 6007])
-  set.seed(2020)
+svm_train <- function(df, opt.gamma, opt.cost){
+  set.seed(0)
   return(
-    svm (x = trainx,
-         y = trainy,
-         kernel = "sigmoid",
-         cross = 50
+    svm (factor(df$label) ~ .,
+         data = df,
+         gamma = opt.gamma,
+         cost = opt.cost,
+         kernel = "linear",
+         cross = 10
     )
   )
 }
 
 svm_test <- function(model, testdf){
-  test.x = NormalizeObject(testdf[ , -6007])
-  set.seed(2020)
+  test.x <- as.matrix(testdf[, -6007])
+  set.seed(0)
   return(
     predict(model, test.x)
   )
